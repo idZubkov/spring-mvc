845e23ffc8917d84cd690fb17dfa20e6d3560760
Andrey Zubkov <zubkoff.id@gmail.com>
2021-03-19 16:45:48 +0300

    cars.html don't work

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -1,9 +1,11 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +13,18 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    @GetMapping(value = "/cars")
-    public List<Car> printCarList(ModelMap modelMap) {
+    @Autowired
+    CarService carService;
+
+    @GetMapping("/cars")
+    public String printCarList(ModelMap modelMap, int count) {
         List<Car> carList = new ArrayList<>();
         carList.add(new Car("BMW", "Blue color", 3));
         carList.add(new Car("Dodge", "Black color", 55));
         carList.add(new Car("Mazda", "Red color", 15));
         carList.add(new Car("Toyota", "Brown color", 18));
         carList.add(new Car("Opel", "Grey color", 9));
-        modelMap.addAttribute("carList", carList);
-        return carList;
+        modelMap.addAttribute("cars", carService.carsFromTheList(carList, count));
+        return "cars";
     }
 }
